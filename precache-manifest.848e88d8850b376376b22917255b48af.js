e179e05abce9ac26284d908e42619e304d0ca3df
dev_seung2 <dev_seung2@naver.com>
2020-01-17 03:30:54 +0900

    Updates

---
+++ precache-manifest.848e88d8850b376376b22917255b48af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78d5b0cdc34e8ce48bea0a139791674",
+    "url": "/findseung2/index.html"
+  },
+  {
+    "revision": "b3b38a0e8937726681b1",
+    "url": "/findseung2/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "16a1f9f50430a5a90628",
+    "url": "/findseung2/static/css/main.f1003efa.chunk.css"
+  },
+  {
+    "revision": "b3b38a0e8937726681b1",
+    "url": "/findseung2/static/js/2.26faa55f.chunk.js"
+  },
+  {
+    "revision": "6d21534beb347a40226ca567c89690f8",
+    "url": "/findseung2/static/js/2.26faa55f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16a1f9f50430a5a90628",
+    "url": "/findseung2/static/js/main.c569de49.chunk.js"
+  },
+  {
+    "revision": "64430eae19b888e716ba",
+    "url": "/findseung2/static/js/runtime-main.a8dce074.js"
+  }
+]);
