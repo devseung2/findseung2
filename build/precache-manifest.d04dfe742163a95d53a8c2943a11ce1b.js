6461ff65d3e94381dc42f60490c482ad9a786c54
dev_seung2 <dev_seung2@naver.com>
2020-02-16 22:35:53 +0900

    SEUNG2 Portfolio

---
+++ build/precache-manifest.d04dfe742163a95d53a8c2943a11ce1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33974d4638fca312609ff7c7f2d7e926",
+    "url": "/findseung2/index.html"
+  },
+  {
+    "revision": "f5feab106f785eaff58f",
+    "url": "/findseung2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "37d3da6173f64617cd2d",
+    "url": "/findseung2/static/css/main.48bdfc41.chunk.css"
+  },
+  {
+    "revision": "f5feab106f785eaff58f",
+    "url": "/findseung2/static/js/2.cf9447dc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/findseung2/static/js/2.cf9447dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37d3da6173f64617cd2d",
+    "url": "/findseung2/static/js/main.57cc4b14.chunk.js"
+  },
+  {
+    "revision": "64430eae19b888e716ba",
+    "url": "/findseung2/static/js/runtime-main.a8dce074.js"
+  }
+]);
